44c4426e69e0e2eb3123953eb58da2e41cb0a710
SlovEnt <slovent@163.com>
2019-06-24 23:06:56 +0800

    Initial commit

---
+++ chs_tools/comm_funcs.py
@@ -0,0 +1,298 @@
+# -*- coding: utf-8 -*-
+__author__ = 'SlovEnt'
+__date__ = '2018/4/28 21:36'
+__version__ = '1.1'
+
+
+import datetime
+import re
+import os
+import time
+import paramiko
+from openpyxl import load_workbook
+from random import Random
+from collections import OrderedDict
+
+def get_now_date(dateFormat):
+
+    nowDate = datetime.now()
+    Y = nowDate.strftime("%Y")
+    M = nowDate.strftime("%m")
+    D = nowDate.strftime("%d")
+
+    if dateFormat == "y-m-d" or dateFormat == "Y-M-D":
+        strDate = str(Y)+"-"+str(int(M))+"-"+str(int(D))
+    elif dateFormat == "yyyy-mm-dd" or dateFormat == "YYYY-MM-DD":
+        strDate = str(Y)+"-"+str(M)+"-"+str(D)
+    elif dateFormat == "yyyymmdd" or dateFormat == "YYYYMMDD":
+        strDate = str(Y)+str(M)+str(D)
+    elif dateFormat == "yyyymmdd_HHMMSS" or dateFormat == "YYYYMMDD_HHMMSS":
+        strDate = nowDate.strftime("%Y%m%d_%H%M%S")
+    else:
+        return 'Get_Now_Date("%s"),入参无效' % dateFormat
+
+    return strDate
+
+def dict2list(dic:dict):
+    ''' 将字典转化为列表 '''
+    keys = dic.keys()
+    vals = dic.values()
+    lst = [(key, val) for key, val in zip(keys, vals)]
+    return lst
+
+def comp_multiline_text(multStr):
+    ''' 压缩多行文本代码，主要用于多行SQL压缩为一行 '''
+    line = multStr.splitlines()
+    ''' 如果行数不仅仅只为一行，则进行多行处理模式 '''
+    if len(line) != 1 :
+        newSql = ""
+        for x in line:
+            newSql = newSql + " " + x.lstrip()
+            newSql = re.sub(r'\s{2,}',' ',newSql)
+        return newSql.lstrip().rstrip()
+    else:
+        ''' 单行只需要去除前后空格即可 '''
+        return multStr.lstrip().rstrip()
+
+def get_trading_day():
+    # 获取交易日,通过组策略下发的文本文件
+    todayYMD = time.strftime("%Y%m%d", time.localtime())  # YYYYMMDD
+    tradeDayFile = r"C:\Windows\HolidayList.txt"
+    fileRdade = open(tradeDayFile)
+    line = fileRdade.readline()
+    while line:
+        strFlag = line.split(" ")
+        if todayYMD == strFlag[0] and int(strFlag[1]) == 2:
+            return False
+        line = fileRdade.readline()
+    fileRdade.close()
+    return True
+
+def get_last_trading_day(dfDay):
+    # 获取交易日,通过组策略下发的文本文件取上一个交易日
+
+    nowDay = datetime.datetime.now()
+
+    deltaDayNum = int(dfDay)
+
+    ''' 日历由域控下发，不在域控的机器需要手工拷贝文件到该目录 '''
+    tradeDayFile = r"C:\Windows\HolidayList.txt"
+    fileRdade = open(tradeDayFile)
+
+    dayFlagArr = []
+    for line in fileRdade.readlines():
+        ''' 合并字典日期和节假日标志，保持为无空格字符串用于匹配比对 '''
+        dayFlag = line.strip("\n").replace(" ", "")
+        dayFlagArr.append(dayFlag)
+
+    while True :
+
+        ''' 日期计算 得出指定的上一个交易日 '''
+        deltaDay = datetime.timedelta(days=deltaDayNum)
+        lastTradeDay = nowDay - deltaDay
+        lastTradeDayX = lastTradeDay.strftime('%Y%m%d')
+        lastTradeDay = "%s1" % lastTradeDay.strftime('%Y%m%d') # YYYYMMDDF F=标志（1或2）
+
+        if lastTradeDay in dayFlagArr:
+            ''' 如果计算出的交易日加标识包含在数组内，则认为当前日期为交易日 '''
+            return lastTradeDayX
+        elif deltaDayNum > 365 :
+            ''' 如果循环日期超过365次，不可能会出现连续放假365天的可能，则基本认为是交易日历有问题 '''
+            return False
+
+        ''' 如果是非交易日，则计数累计加1 取再上一天 '''
+        deltaDayNum = deltaDayNum + 1
+
+    return False
+
+def print_dict_item(dict,dictName="x"):
+    for key, value in dict.items():
+        print ("""{2}["{0}"] = {1}""".format(key, value, dictName))
+    print()
+    time.sleep(0.5)
+
+def get_linux_env_nls_lang(sshCmd):
+    '''取Oracle用户下的字符集编码'''
+    '''需要实例化sshCmd'''
+    strCMD = "cat ~/.bash_profile |grep NLS_LANG"
+    stdin, stdout, stderr = sshCmd.exec_command(strCMD)
+    for std in stdout.readlines():
+        if "#" not in std.strip("\n"):
+            dataReg = re.compile(r"""export NLS_LANG=["|'](.+?)['|"]""")
+            rawDatas = dataReg.findall(std.strip("\n"))
+            if len(rawDatas) == 1:
+                return rawDatas[0]
+    return False
+
+def get_exclude_list(xlsFile,xlsSheetName):
+    # 读取Excek文件
+    xlsWBook = load_workbook(xlsFile)
+    xlsWSheet = xlsWBook.get_sheet_by_name(xlsSheetName)
+    startRow = 2  # 起始行号
+    xlsRowList = []
+
+    colCnt = len(list(xlsWSheet.columns))
+    rowCnt = len(list(xlsWSheet.rows))
+    xlsWSheet = list(xlsWSheet.rows)
+
+    xSn = 0
+    for rowv in range(1, rowCnt):
+        xSn = xSn + 1
+        colDict={}
+        for colv in range(0, colCnt):
+            if xlsWSheet[rowv][colv].value == None:
+                valueX = ""
+            else:
+                valueX = xlsWSheet[rowv][colv].value
+
+                # 如果Excel里的变量为字符型，则去前后空格，减免出错几率
+                if isinstance(valueX,str) == True:
+                    valueX = valueX.strip()
+
+            colDict[xlsWSheet[0][colv].value] = valueX
+            colDict["xSn"] = xSn
+        if int(colDict["处理标志"][0]) == 0:
+            xlsRowList.append(colDict)
+
+    return xlsRowList
+
+def get_ssh_hander(hostIP, userName, userPasswd):
+    sshCmd = paramiko.SSHClient()
+    sshCmd.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    sshCmd.connect(hostIP, 22, userName, userPasswd)
+    return sshCmd
+
+def Net_RemtePath_IsAccess(remoteNetPath, remoteHostUser, remoteHostUserPasswd):
+
+    strCmd = r"net use %s %s /user:%s" % (remoteNetPath, remoteHostUserPasswd, remoteHostUser)
+
+    try:
+        from subprocess import PIPE, Popen
+        cmdHandle = Popen(strCmd, shell=True, stdout=PIPE, stderr=PIPE)
+        out, err = cmdHandle.communicate()
+        strOutMsg = out.decode("gb2312")
+        strErrMsg = err.decode("gb2312")
+        if strOutMsg:
+            rtnCmdMsg = strOutMsg
+        else:
+            rtnCmdMsg = strErrMsg
+
+        rtnCmdMsgLine = ""
+        for line in rtnCmdMsg:
+            rtnCmdMsgLine += line.strip()
+
+        if "1219" in rtnCmdMsgLine or strOutMsg:
+            # 尝试文件是否可以进行读写操作
+            import uuid
+            wirteFlagFile = "%s\%s" % (remoteNetPath,str(uuid.uuid4()))
+            with open(wirteFlagFile, "w") as testFile:
+                pass
+            os.remove(wirteFlagFile)
+            return True
+        else:
+            return rtnCmdMsgLine
+
+    except Exception as e:
+        return e
+
+def Get_Random_Str(randomlength=8):
+    str = ''
+    chars = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz013456789'
+    length = len(chars) - 1
+    random = Random()
+    for i in range(randomlength):
+        str += chars[random.randint(0, length)]
+    return str
+
+# /*  以下函数为暂存，无法直接使用。
+def Table_Row_Is_Exist(self, tableName, whereDict):
+    ''' 返回指定条件下返回的ROW数据总数 '''
+    strSql = "SELECT * FROM {0} WHERE 0=0".format(tableName)
+    strWhere = ""
+    for key, value in whereDict.items():
+        strWhere = strWhere + " " + "AND {0}='{1}'".format(key, value)
+
+    strSql = strSql + strWhere
+
+    rtnDatas = OrderedDict()
+
+    # print(strSql)
+    rtnDBDatas = self.mysqlConn.query(strSql)
+
+    rtnDatas["CNT"] = len(rtnDBDatas)
+    rtnDatas["CONTENT"] = rtnDBDatas
+    # rtnDatas["STRWHERE"] = strWhere.replace(strWhere[0:4], "")
+    rtnDatas["STRSQL"] = strSql
+
+    return rtnDatas
+
+def Update_Init_TaskLog(self,tableName, whereDict, newSetData):
+    ''' 入参格式 第一个字典为条件字典 第二个是SET字段（字典格式） '''
+
+    # 分拆字典 把原始字典分拆为两个SQL语句段 条件 SET
+    strWhereSql = ""
+    strSetSql = ""
+    strQuerySql = ""
+
+    for setField in newSetData:
+
+        # 删除folder_name字典项 数据库中无此列表
+        if setField == "folder_name":
+            continue
+
+        ''' 组装SET语句 '''
+        for key, value in newSetData.items():
+            if isinstance(value,str) == True :
+                value = value.replace("'", "''")
+            if key == setField:
+                # SET 语句
+                if strSetSql == "":
+                    strSetSql = "%s='%s'" % (key, value)
+                    strQuerySql = "%s" % (key)
+                else:
+                    strSetSql = strSetSql + ", %s='%s'" % (key, value)
+                    strQuerySql = strQuerySql + ", %s" % (key)
+
+    for setField in whereDict:
+        ''' 组装WHERE语句 '''
+        for key, value in whereDict.items():
+            if isinstance(value,str) == True :
+                value = value.replace("'", "''")
+            if key == setField:
+                # SET 语句
+                if strWhereSql == "":
+                    strWhereSql = " AND %s='%s'" % (key, value)
+                else:
+                    strWhereSql = strWhereSql + "AND %s='%s'" % (key, value)
+
+    ''' 最终组装UPDATE全语句 '''
+    strSql = "UPDATE {0} SET {1} WHERE 0=0{2}".format(tableName, strSetSql, strWhereSql)
+    # print(strSql)
+
+    try:
+        self.mysqlConn.execute(strSql)
+        return True
+    except Exception as e:
+        print(e)
+        return False
+
+def Inser_Init_TaskLog(self, tableName, postNode):
+    fieldList = ""
+    valueList = ""
+    for key, value in postNode.items():
+        if key == "folder_name":
+            continue
+        if fieldList == "":
+            fieldList = "{0}".format(key)
+            valueList = "'{0}'".format(value)
+        else:
+            fieldList = "{0}, {1}".format(fieldList, key)
+            valueList = "{0}, '{1}'".format(valueList, value)
+    strSql = "INSERT INTO {0} ({1}) VALUES ({2})".format(tableName, fieldList, valueList)
+    # print(strSql)
+    try:
+        self.mysqlConn.execute(strSql)
+        return True
+    except Exception as e:
+        return e
+# ------------------------------------------------------------------*/
---
+++ crawler_novels/www.ggdown.com.py
@@ -0,0 +1,144 @@
+# -*- coding: utf-8 -*-
+__author__ = 'SlovEnt'
+__date__ = '2019/6/24 22:07'
+
+import time
+import os
+from bs4 import BeautifulSoup
+from collections import OrderedDict
+from chs_tools.get_html_page import get_html_all_content, chrome_get_html_all_content
+from chs_tools.print_log import C_PrintLog
+from chs_tools.param_info import rtn_parainfo
+import traceback
+
+
+plog = C_PrintLog()
+PARAINFO = rtn_parainfo()
+DOWN_FLODERS = PARAINFO["NOVEL_DOWN_FLODERS"]
+
+ROOT_URL = "http://www.ggdown.com" # 网站根目录
+GENERAL_PATH = ""                      # 通用路径
+NOVEL_SUB_ID = "29/29516"              # 目录页面ID
+ENCODING = "GBK"                    # 页面文字编码
+CHAPTER_POST = 1
+"http://www.ggdown.com/29/29516/index.html"
+if GENERAL_PATH == "":
+    FULL_URL = "{0}/{1}/index.html".format(ROOT_URL, NOVEL_SUB_ID)
+else:
+    FULL_URL = "{0}/{1}/{3}/index.html".format(ROOT_URL, GENERAL_PATH, NOVEL_SUB_ID)
+plog.debug("小说下载首页为：{0}".format(FULL_URL))
+
+
+def rtn_chapter_list_info(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    novelName = soup.find_all(name="div", attrs={"class": "btitle"})[0].h1.text
+    # novelName = novelName.split("《")[1]
+    # novelName = novelName.split("》")[0]
+    # novelName = "妾本惊华"
+    plog.debug("开始下载《{0}》".format(novelName))
+
+    chapterListInfoSoup = soup.find_all(name="dd")
+    # print(chapterListInfoSoup)
+
+    chapterListInfoArr = []
+
+    n = 0
+    for ddItem in chapterListInfoSoup:
+        # print(ddItem)
+        n += 1
+
+        # if n <= 12:
+        #     continue
+
+        chapterListInfoDict = OrderedDict()
+        chapterListInfoDict2 = OrderedDict()
+
+        if "href" not in str(ddItem):
+            continue
+
+        if n < CHAPTER_POST:
+            continue
+
+        chapterListInfoDict["text"] = ddItem.a.text
+        chapterListInfoDict["href"] = ddItem.a["href"]
+        chapterListInfoArr.append(chapterListInfoDict)
+
+        # chapterListInfoDict2["text"] = "接"
+        # nextPageUrl = ddItem.a["href"].split(".")
+        # nextPageUrl = "{0}_2.{1}".format(nextPageUrl[0], nextPageUrl[1])
+        # chapterListInfoDict2["href"] = nextPageUrl
+        # chapterListInfoArr.append(chapterListInfoDict2)
+
+        plog.tmpinfo(chapterListInfoDict)
+
+    return chapterListInfoArr, novelName
+
+def rtn_chapter_txt(chapterHtml):
+
+
+    # print("---------------chapterHtml-----------------\n",chapterHtml,"\n\n\n\n")
+    # chapterHtml = chapterHtml.replace("<br />", "\n")
+
+    soup = BeautifulSoup(chapterHtml, 'html.parser')
+
+    try:
+        soupSub = soup.find_all(name="div", attrs={"id": "pagecontent"})[0]
+        # soupSubStr = str(soupSub)
+        # print("---------------soupSubStr-----------------\n",soupSubStr,"\n\n\n\n")
+        # soupSubStr = "{0}{1}".format(soupSubStr.split("<div")[0],"</article>")
+
+        # soupSub = BeautifulSoup(soupSubStr, 'html.parser')
+
+        txtContent = soupSub.text
+        txtContent = txtContent.replace("    ", "")
+        txtContent = txtContent.replace("                ", "")
+        txtContent = txtContent.replace("\n\n", "\n")
+        txtContent = txtContent.replace("记住我们的网址噢。百度搜;格！！格！！党.或者直接输域名/g/g/d/o/w/n/./c/o/m", "")
+
+        # txtContent = txtContent.split("/c/o/m")[1] + "\n"
+        print(txtContent)
+        return txtContent
+
+    except:
+        time.sleep(2)
+        traceback.print_exc()
+        print("--------------- chapterHtml error -----------------\n", chapterHtml)
+        return False
+
+def write_txt_content(txtFileName, chapterName, chapterTxt, encoding):
+    with open(txtFileName, 'a', encoding=encoding) as f:
+        if chapterName == "接":
+            pass
+        else:
+            f.write(chapterName + "\n")
+        # print(chapterTxt)
+        f.write(chapterTxt)
+
+if __name__ == '__main__':
+
+    html = chrome_get_html_all_content(FULL_URL, "bookinfo", ENCODING)
+
+    # 返回章节信息
+    chapterListInfo, novelName = rtn_chapter_list_info(html)
+
+    novelFilePath = r"{0}\{1}.txt".format(DOWN_FLODERS, novelName)
+
+    if CHAPTER_POST == 1:
+        if (os.path.exists(novelFilePath)):
+            os.remove(novelFilePath)
+
+    for chapterInfo in chapterListInfo:
+
+        chapterUrl = "{0}/{1}/{2}".format(ROOT_URL, NOVEL_SUB_ID, chapterInfo["href"])
+
+        plog.debug("网址：{0}，页面章节标题：{2}，文件路径：{1} ！！！".format(chapterUrl, novelFilePath, chapterInfo["text"]))
+
+        chapterHtml = chrome_get_html_all_content(chapterUrl, "pagecontent", ENCODING)
+
+        chapterTxt = rtn_chapter_txt(chapterHtml)
+        # print(str(chapterHtml))
+
+        if chapterTxt is not False:
+            write_txt_content(novelFilePath, chapterInfo["text"], chapterTxt, ENCODING)
+        else:
+            plog.error("获取失败！！！！！！")
